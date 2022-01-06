6c0fd317419e34edbb7f78f64c167498bef87b4a
ThatGuy2001 <rodrigues.diogofilipe@gmail.com>
2022-01-06 19:03:35 +0000

    Manutenção do menu e alterações minimas em funçoes player Habilidade tempuraria de escrever um fichero jogadas.txt

--- code/othello_func.h
+++ code/othello_func.h
@@ -10,7 +10,7 @@ int check(char board[9][9],int l,int c,int linha,int col,char player,int *p);
 
 void play(char board[9][9],char player, int linha, int coluna);
 
-char player(int turn,int ordem);
+char player(int turn);
 
 void input(int *linha,int *col);
 
